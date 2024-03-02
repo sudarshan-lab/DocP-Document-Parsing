9293c505d16bedc4578f50f5bd58d4b39b5d4f0d
Mayurarora123 <aroramayur20@gmail.com>
2024-03-02 10:25:32 -0500

    Config changes for PostgreSQL

--- text_extraction/text_extraction/settings.py
+++ text_extraction/text_extraction/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'text_extraction.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'docp',
+        'USER': 'postgres',
+        'PASSWORD': '14911912',
+        'HOST': 'localhost', 
+        'PORT': '5432', 
     }
 }
 
