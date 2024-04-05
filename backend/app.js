9ad0e10cec585cde749abd4d216aea9b854190d1
Kammu Sudarshan <skammu@albany.edu>
2024-04-05 17:38:23 -0400

    Initial folder requirements and s3 upload code update

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,38 @@
 const fs = require('fs');
 const express = require('express');
+const cors = require("cors");
+const dotenv = require('dotenv');
 const app = express();
+app.use(express.json());
+dotenv.config();
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, 'uploads/') // specify the directory where uploaded files will be stored
+  },
+  filename: function (req, file, cb) {
+      cb(null, file.originalname) // use the original file name for storing
+  }
+});
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_ACCESS_SECRETKEY,
+  region: process.env.AWS_REGION,
+});
+
+const S3Upload = async (file,fileContent) => {
+  const params = {
+    Key: `${file.originalname}`,
+    Bucket: process.env.AWS_BUCKET_NAME,
+    ContentType: file.mimetype,
+    Body: fileContent,
+    ACL: "public-read",
+  };
+  return await s3.upload(params).promise();
+};
+
   app.post('/upload', upload.single('file'), async (req, res) => {
     try {
       const filePath = req.file.path;
