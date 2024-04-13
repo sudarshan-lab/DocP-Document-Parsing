78b90a140e524c05ba9fad5cf9ad6e9d8a5fefba
SaiManojK16 <skartala@albany.edu>
2024-04-13 16:40:25 -0400

    Routing for User Log Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/login/Login";
+import UserInformationPage from "./components/UserInformationPage/UserInformationPage";
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/userInformationPage" element={<UserInformationPage />} />
         </Routes>
       </Router>
     </div>
