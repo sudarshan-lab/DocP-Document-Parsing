8b2b5d33dc32f3181b30e60544888c2229b8747c
sjatling36 <157991464+sjatling36@users.noreply.github.com>
2024-04-13 14:41:29 -0400

    UI changes for modified frontend

---
+++ client/src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import "semantic-ui-css/semantic.min.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
