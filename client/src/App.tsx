6f29ecd6fb9e73775e5d9cd2c3ea7ba7f14ba969
tchennupati@albany.edu <tchennupati@albany.edu>
2024-04-17 11:29:27 -0400

    Setting Up Routing Config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
-import Login from "./components/login/Login";
-import UserInformationPage from "./components/UserInformationPage/UserInformationPage";
+import Dashboard from "./components/dashboard/Dashboard";
+import UploadUsers from "./components/users-upload/UploadUsers";
 import Announcement from "./components/announcements/Announcement";
+import Login from "./components/login/Login";
+import AdminDashboard from "./components/dashboard/AdminDashboard";
+import History from "./components/history/History";
+import UserInformationPage from "./components/UserInformationPage/UserInformationPage";
 function App() {
   return (
     <div className="App">
-
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<AdminDashboard />} />
+          <Route path="/history" element={<History />} />
           <Route path="/contract" element={<Announcement />} />
+          <Route path="/fileupload" element={<UploadUsers />} />
           <Route path="/userInformationPage" element={<UserInformationPage />} />
         </Routes>
       </Router>
